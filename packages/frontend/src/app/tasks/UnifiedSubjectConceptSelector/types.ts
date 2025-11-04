e33c5a2485f8b5936a81f5c18fb98bdf586a5c56
Maik <maibaum@lamapoll.de>
2025-11-04 22:51:44 +0100

    feat: add types for unified selector

---
+++ packages/frontend/src/app/tasks/UnifiedSubjectConceptSelector/types.ts
@@ -0,0 +1,24 @@
+import { SubjectInfo, ConceptInfo } from '@/api/logikids'
+
+export interface UnifiedSubjectConceptSelectorProps {
+  subject: string
+  concept?: string
+  subjects: SubjectInfo[]
+  onSubjectChange: (subject: string) => void
+  onConceptChange: (concept: string) => void
+}
+
+export interface SubjectListProps {
+  subjects: SubjectInfo[]
+  currentSubject: string
+  previewSubject: string
+  onSubjectClick: (subjectId: string) => void
+  onSubjectHover: (subjectId: string) => void
+}
+
+export interface ConceptListProps {
+  subject: string
+  concepts: ConceptInfo[]
+  currentConcept?: string
+  onConceptClick: (conceptId: string) => void
+}
